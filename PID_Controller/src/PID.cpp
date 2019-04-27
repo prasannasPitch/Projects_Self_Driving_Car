763e7e6c34548109eaad281604223bba4775b09e
Prasanna <prasanna.mseceee@gmail.com>
2019-04-27 12:58:29 +0200

    Updated with working copy of PID controller

--- PID_Controller/src/PID.cpp
+++ PID_Controller/src/PID.cpp
@@ -1,30 +1,97 @@
 #include "PID.h"
-
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
+#include <cmath>		
+#include <iostream>
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+   Kp = Kp_;		
+   Ki = Ki_;		
+   Kd = Kd_;		
+   p_error = 0;		
+   i_error = 0;		
+   d_error = 0;		
+   dpp = 0.005;		
+   dpi = 0.0001;		
+   dpd = 0.05;		
+   controller = 0;		
+   controllerSequence = 0;		
+   best_err_initialized = false;
 
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+	d_error = cte - p_error;		
+    p_error = cte;		
+    i_error += cte;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+return  -Kp * p_error - Ki * i_error - Kd * d_error;		  return 0.0;  // TODO: Add your total error calc here!
+}		
+double * PID::setGainValue(const int& controller) {		
+    switch (controller) {		
+        case 0:		
+            return &Kp;		
+        case 1:		
+            return &Ki;		
+        case 2:		
+            return &Kd;		
+        default:		
+            std::cout<<"Gain sequence not in sync!"<<std::endl;			
+    }		
+}
+
+
+double * PID::setDifferentialValue(const int& controller) {		
+    switch (controller) {		
+        case 0:		
+            return &dpp;		
+        case 1:		
+            return &dpi;		
+        case 2:		
+            return &dpd;		
+        default:		
+            std::cout<<"Differential sequence not in sync!"<<std::endl;	
+    }		
+}		
+void PID::twiddle(double err) {			
+
+    double* K = setGainValue(controller);
+    double* dp = setDifferentialValue(controller);
+
+    switch (controllerSequence) {	
+        case 0:		
+            (*K) += (*dp);		
+            controllerSequence++;		
+            break;		
+        case 1:		
+            if (err < best_err) {		
+                best_err = err;		
+                (*dp) *= 1.1;			
+                controller = (controller >= 2) ? 0 : (controller+1);		
+                controllerSequence = 0;		
+            }		
+            else {		
+                (*K) -= 2 * (*dp);		
+                controllerSequence++;		
+            }		
+            break;		
+        case 2:		
+            if (err < best_err) {		
+                best_err = err;		
+                (*dp) *= 1.1;		
+            }		
+            else {		
+                (*K) += (*dp);		
+                (*dp) *= 0.9;		
+            }		
+            controller = (controller >= 2) ? 0 : (controller+1);		
+            controllerSequence = 0;		
+            break;		
+        default:		
+            std::cout<<"PID sequence not in sync!"<<std::endl;	
+    }		
 }
